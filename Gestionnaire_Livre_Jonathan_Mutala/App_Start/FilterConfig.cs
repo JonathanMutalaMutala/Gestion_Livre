77ede3dd1563e9a2fa510825a02854c878fb9d0e
JonathanMutala <jonamut12.live@gmail.com>
2021-08-05 22:04:16 -0300

    Ajout du projet

---
+++ Gestionnaire_Livre_Jonathan_Mutala/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Gestionnaire_Livre_Jonathan_Mutala
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Gestionnaire_Livre_Jonathan_Mutala/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Gestionnaire_Livre_Jonathan_Mutala.MvcApplication" Language="C#" %>
